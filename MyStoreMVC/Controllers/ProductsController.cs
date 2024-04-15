e17ef3ac48ba505f840e679959eebf42b9f8ce8e
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-04-15 20:32:43 +1200

    Read and Display Product

---
+++ MyStoreMVC/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyStoreMVC.Services;
+
+namespace MyStoreMVC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var products = _context.Products.OrderByDescending(p => p.Id).ToList();
+            return View(products);
+        }
+    }
+}
