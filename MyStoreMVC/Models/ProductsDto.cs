3796e457506adabcb96299fb36a2147f415e798c
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-04-15 20:38:03 +1200

    Add ProductsDto

--- MyStoreMVC/Models/ProductsDto.cs
+++ MyStoreMVC/Models/ProductsDto.cs
@@ -3,26 +3,22 @@ using System.ComponentModel.DataAnnotations;
 
 namespace MyStoreMVC.Models
 {
-    public class Product
+    public class ProductsDto
     {
-        public int Id { get; set; }
-
-        [MaxLength(100)]
+        [Required, MaxLength(100)]
         public string Name { get; set; } = "";
 
-        [MaxLength(100)]
+        [Required, MaxLength(100)]
         public string Brand { get; set; } = "";
 
-        [MaxLength(100)]
+        [Required, MaxLength(100)]
         public string Category { get; set; } = "";
 
-        [Precision(16, 2)]
+        [Required]
         public decimal Price { get; set; }
+        [Required]
         public string Description { get; set; } = "";
-
-        [MaxLength(100)]
-        public string ImageFileName { get; set; } = "";
-        public DateTime CreatedAt { get; set; }
+        public IFormFile? ImageFile { get; set; } 
 
     }
 }
